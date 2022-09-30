a0bbc963ee88996794e835ae2aa72140a920141c
Coen <code.github@coenvs.nl>
2022-09-30 14:44:34 +0200

    Fix clearing result paragraphs, add no-input warning

--- 03-learn-js/7-practice-time-3/solo-project-unit-converter/index.js
+++ 03-learn-js/7-practice-time-3/solo-project-unit-converter/index.js
@@ -1,35 +1,56 @@
 const convertBtn = document.getElementById('convert-btn');
 const inputEl = document.getElementById('input');
-const lengthResult = document.getElementById('length');
-const volumeResult = document.getElementById('volume');
-const massResult = document.getElementById('mass');
+const lengthResults = document.getElementById('length-results');
+const volumeResults = document.getElementById('volume-results');
+const massResults = document.getElementById('mass-results');
+const noInputWarning = document.getElementById('no-input-warning');
 
 convertBtn.addEventListener('click', function() {
+  clearAll();
   const input = inputEl.value;
+  
+  if (!input) {
+    noInput();
+    return;
+  }
+
   renderLength(input);
   renderVolume(input);
   renderMass(input);
+  clearInputEl();
 });
+
+const clearInputEl = () => inputEl.value = '';
+const clearAll = () => {
+  noInputWarning.textContent = '';
+  lengthResults.innerHTML = '';
+  volumeResults.innerHTML = '';
+  massResults.innerHTML = '';
+}
+
+const noInput = () => {
+  noInputWarning.textContent = 'Please type a number!';
+}
 
 function renderLength(num) {
   const feet = metersToFeet(num);
   const meters = feetToMeters(num);
-  let result = `<p>${num} meters = ${feet} feet | ${num} feet = ${meters} meters</p>`;
-  lengthResult.innerHTML += result;
+  let result = `${num} meters = ${feet} feet | ${num} feet = ${meters} meters`;
+  lengthResults.innerHTML += result;
 }
 
 function renderVolume(num) {
   const gallons = litersToGallons(num);
   const liters = gallonsToLiters(num);
-  let result = `<p>${num} liters = ${gallons} | ${num} gallons = ${liters}</p>`;
-  volumeResult.innerHTML += result;
+  let result = `${num} liters = ${gallons} | ${num} gallons = ${liters}`;
+  volumeResults.innerHTML += result;
 }
 
 function renderMass(num) {
   const pounds = kilosToPounds(num);
   const kilos = poundsToKilos(num);
-  let result = `<p>${num} kilos = ${pounds} pounds | ${num} pounds = ${kilos} kilos</p>`;
-  massResult.innerHTML += result;
+  let result = `${num} kilos = ${pounds} pounds | ${num} pounds = ${kilos} kilos`;
+  massResults.innerHTML += result;
 }
 
 const metersToFeet = num => (num * 3.281).toFixed(3);
